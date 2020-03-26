7483945f0a5dda67fcc53d2f545216e54216a2ca
Mohammad Wira Mardhotillah <muhammadwira@github.com>
2020-03-26 17:54:00 +0700

    forgot to update the database

--- projects_db.sql
+++ projects_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2020 at 03:12 AM
+-- Generation Time: Mar 26, 2020 at 11:52 AM
 -- Server version: 10.1.8-MariaDB
 -- PHP Version: 5.6.14
 
@@ -19,6 +19,75 @@ SET time_zone = "+00:00";
 --
 -- Database: `projects_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat_message`
+--
+
+CREATE TABLE `chat_message` (
+  `chat_message_id` int(11) NOT NULL,
+  `to_user_id` int(11) NOT NULL,
+  `from_user_id` int(11) NOT NULL,
+  `chat_message` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`customer_id`, `email`, `username`, `password`) VALUES
+(1, 'mohammadwira@gmail.com', 'Mohammad Wira', '$2y$10$lHJ.d6V3Jj0j4tJlrp3jT.Lp6175HHP250a1XbPp3NwiYzxBYSSFC');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers_details`
+--
+
+CREATE TABLE `customers_details` (
+  `customer_detail_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `last_activity` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_type` enum('no','yes') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `student_id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`student_id`, `name`, `email`, `password`) VALUES
+(3, 'Mohammad Wira', 'mohammadwira@gmail.com', '$2y$10$GRXHm6Y40.eMWGE0jqfn9uGXTvWbtoV5KSsLZPKyovy4UiXroqMj6'),
+(4, 'Sukainah Shireen', 'sukainahshireen@gmail.com', '$2y$10$jZk5JArFD3K06Lwh9n57rOfGYu7nOIVertpnYSVuMPh4gQ.5I2a7W');
 
 -- --------------------------------------------------------
 
@@ -39,12 +108,36 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `name`, `email`, `phone`) VALUES
 (1, 'Wira Mardhotillah', 'mohammadwira@gmail.com', '082242426018'),
-(2, 'Nira MBUL', 'niranurfadzilah@gmail.com', '087736452364'),
-(6, 'Nola Anggun', 'nolanggun@gmail.com', '082237485748');
+(2, 'Nira Nurfadzilah', 'niranurfadzilah@gmail.com', '087736452364'),
+(7, 'Anggun Safitri', 'anggunsafitri@gmail.com', '085741236985');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `chat_message`
+--
+ALTER TABLE `chat_message`
+  ADD PRIMARY KEY (`chat_message_id`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `customers_details`
+--
+ALTER TABLE `customers_details`
+  ADD PRIMARY KEY (`customer_detail_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_id`);
 
 --
 -- Indexes for table `users`
@@ -57,10 +150,30 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `chat_message`
+--
+ALTER TABLE `chat_message`
+  MODIFY `chat_message_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `customers_details`
+--
+ALTER TABLE `customers_details`
+  MODIFY `customer_detail_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
